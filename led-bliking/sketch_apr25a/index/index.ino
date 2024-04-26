19824efa009b160983b6134adacca4e240242e71
samhayder <sams.seul@gmail.com>
2024-04-26 21:30:22 +0600

    LED Light Bliking project

--- led-bliking/sketch_apr25a/index/index.ino
+++ led-bliking/sketch_apr25a/index/index.ino
@@ -1,2 +1,13 @@
-int value;
-int volt;
+int led = 13;
+int inp = 2;
+int state;
+
+void setup(){
+  pinMode(led, OUTPUT);
+  pinMode(inp, INPUT);
+}
+
+void loop(){
+  state = digitalRead(inp);
+  digitalWrite(led, state);
+}
